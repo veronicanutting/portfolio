01719cd91329875b5b1a665eb335b4a5c9e3a263
Veronica Nutting <veronicanutting@Veronicas-New-MacBook-Air.local>
2023-02-14 15:03:06 -0800

    cropped photo plus minor navbar change

--- js/navbar.js
+++ js/navbar.js
@@ -7,6 +7,7 @@ document.getElementById('myNavBar').innerHTML =
  </div>`
 
  document.getElementById('myFooter').innerHTML = 
-`<div class="p-2" style="background-color:lightblue;">site by veronica nutting</div>`
-
-// removed <div class="p-2"><a href="https://docs.google.com/spreadsheets/d/1-1J-Do9TP8dymGw4Vr1CHYM18dVo3B3FLOFJDsWT8EA/edit?usp=sharing">art data here</a></div>
+`<div class="p-2" style="background-color:lightblue;">site by veronica nutting</div>
+<div class="p-2" style="background-color:lightblue;">
+<a href="https://docs.google.com/spreadsheets/d/1-1J-Do9TP8dymGw4Vr1CHYM18dVo3B3FLOFJDsWT8EA/edit?usp=sharing">art data here</a>
+</div>`
